2af3f232090e75b5735370335fbc6fd770b37c76
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-07-05 17:09:35 +0100

    7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, str]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """This function takes a string k and an int OR float v as arguments
     and returns a tuple."""
     the_list = [k, v ** 2]
