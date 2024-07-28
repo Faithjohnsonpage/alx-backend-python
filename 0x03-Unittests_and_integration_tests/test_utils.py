5e30d53a9b2921a060099f138887555c8bfedb94
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-07-28 09:12:52 +0100

    Task 1 solution

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,7 @@ import utils
 class TestAccessNestedMap(unittest.TestCase):
     """
     Test case for the access_nested_map function in the utils module.
-    
+
     This class contains unit tests that verify the behavior of the
     access_nested_map function with different nested map inputs and key paths.
     """
@@ -37,3 +37,31 @@ class TestAccessNestedMap(unittest.TestCase):
         matches the expected value for each test case.
         """
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError)
+            ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """
+        Tests the access_nested_map function for exceptions.
+
+        This test method verifies that the access_nested_map function raises
+        the expected exceptions for invalid paths in the nested map.
+
+        Parameters
+        ----------
+        nested_map : dict
+            The nested map to access.
+        path : tuple
+            The sequence of keys representing the path to the value.
+        expected : Exception
+            The expected exception to be raised by access_nested_map.
+
+        Asserts
+        -------
+        The function asserts that the expected exception is raised when
+        accessing an invalid path in the nested map.
+        """
+        with self.assertRaises(expected):
+            utils.access_nested_map(nested_map, path)
