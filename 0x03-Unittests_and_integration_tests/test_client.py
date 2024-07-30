1875ac8a8c9005191dd95985bcff9ef45eee8495
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-07-30 04:31:34 +0100

    Solution task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,3 +36,37 @@ class TestGithubOrgClient(unittest.TestCase):
 
             self.assertEqual(result, "https://api.github.com/orgs/google/repo")
             mock_org.assert_called_once()
+
+    @patch('utils.requests.get')
+    def test_public_repos(self, mock_get):
+        # Define the payload to return from the mocked requests.get
+        payload = [
+            {"name": "repo1", "license": {"key": "mit"}},
+            {"name": "repo2", "license": {"key": "apache-2.0"}},
+            {"name": "repo3", "license": {"key": "gpl-3.0"}},
+        ]
+
+        # Create a mock response object
+        mock_response = Mock()
+        mock_response.json.return_value = payload
+        mock_get.return_value = mock_response
+
+        # Mock _public_repos_url to return a specific URL
+        with patch.object(GithubOrgClient, '_public_repos_url',
+                          new_callable=PropertyMock) as mock_repos_url:
+            mock_repos_url.return_value = \
+                    "https://api.github.com/orgs/google/repos"
+
+            client = GithubOrgClient("google")
+            result = client.public_repos()
+
+            # Expected result from the payload
+            expected_result = ["repo1", "repo2", "repo3"]
+
+            # Assert the result matches the expected result
+            self.assertEqual(result, expected_result)
+
+            # Ensure the mocked property and get_json were called once
+            mock_repos_url.assert_called_once()
+            url = "https://api.github.com/orgs/google/repos"
+            mock_get.assert_called_once_with(url)
